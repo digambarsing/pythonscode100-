febb8c539ff0b371cf5e87f0db71cea4be8c6f3a
Digambar singh <138235609+digambarsing@users.noreply.github.com>
2024-02-16 11:10:02 +0530

    Add files via upload

---
+++ fun1.py
@@ -0,0 +1,21 @@
+number=[1,2,3,4,5]
+cities=['agra','ved','dev','rahul']
+
+
+def length(li):
+    print(len(li))
+
+# length(number)
+
+# length(cities)
+    
+
+def first(li):
+    for item in li:
+        print(item,end=" ")
+
+
+
+first(number)
+print()
+first(cities)
